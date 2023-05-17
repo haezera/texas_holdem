861a2596472cd3a72fc7858612b5793e7cd946b5
haezera <123307411+haezera@users.noreply.github.com>
2023-05-17 11:49:01 +1000

    modulated functions file

---
+++ functions.py
@@ -0,0 +1,90 @@
+import random
+from node import Node
+from linkedlist import LinkedList
+from player import Player
+from opponent import Opponent
+from deck import Deck
+from card import Card
+
+def printc(card):
+    if (card.suit == "spades"):
+        print(" _____ ")
+        print("|" + card.number + " .  |")
+        print("| /.\ |")
+        print("|(_._)|")
+        print("|  |  |")
+        print("|_____|")
+    if (card.suit == "diamonds"):
+        print(" _____ ")
+        print("|" + card.number + " ^  |")
+        print("| / \ |")
+        print("| \ / |")
+        print("|  .  |")
+        print("|_____|")
+    if (card.suit == "clubs"):
+        print(" _____ ")
+        print("|" + card.number + " _  |")
+        print("| ( ) |")
+        print("|(_'_)|")
+        print("|  |  |")
+        print("|_____|")
+    if (card.suit == "hearts"):
+        print(" _____ ")
+        print("|" + card.number + "_ _ |")
+        print("|( v )|")
+        print("| \ / |")
+        print("|  .  |")
+        print("|_____|")
+
+def player_info_gathering():
+    name = input("What is your name? ")
+    player = Player(name)
+    return player
+
+def flop(table, deck, deck_count):
+    # Burn one card
+    deck_count += 1
+    table.append(deck._cards[deck_count])
+    deck_count += 1
+    table.append(deck._cards[deck_count])
+    deck_count += 1
+    table.append(deck._cards[deck_count])
+    deck_count += 1
+    # Printing of cards
+    print("Table:")
+    printc(table[0])
+    printc(table[1])
+    printc(table[2])
+
+def printh(player):
+    print("Your hand:")
+    printc(player.hand[0])
+    printc(player.hand[1])
+
+
+def player_introduction(player):
+    print("Hello, " + player.name + ". Welcome to Texas Holdem.")
+    print("The goal of Texas Holdem is to create the best combination of cards possible. These combinations are ranked such that they are less probable the strong they are.")
+    print("Opponents have a 'bluff' rating.")
+    print("The higher their 'bluff' rating, the more likely it is that they bluff a bad hand.")
+    print("----------------------------------------------------------------------------------")
+
+def deal_cards(current, deck_count, deck, table):
+    while current.player.dealer != 1:
+        current.player.hand.append(deck._cards[deck_count])
+        current = current.next
+        deck_count = deck_count + 1
+    current.player.hand.append(deck._cards[deck_count])
+
+    current = table.head
+    current = current.next
+    # Phase two of dealing.
+    while current.player.dealer != 1:
+        current.player.hand.append(deck._cards[deck_count])
+        current = current.next
+        deck_count = deck_count + 1
+    current.player.hand.append(deck._cards[deck_count])
+
+
+    # Adding probabilities and bluff effect. No bluffing pre-flop, that's stupid.
+    # Perhaps a percentage of the pool v.s their cash should be taken into context. 
