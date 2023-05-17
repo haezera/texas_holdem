be083466d3b76d3de5c93c080581572a091af136
haezera <123307411+haezera@users.noreply.github.com>
2023-05-17 11:43:59 +1000

    bug fix

--- deck.py
+++ deck.py
@@ -9,7 +9,7 @@ class Deck:
     def populate(self):
         suits = ["hearts", "clubs", "diamonds", "spades"]
         numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-        self._cards = [Card(s, n) for s in suits for n in numbers]
+        self._cards = [card(s, n) for s in suits for n in numbers]
 
     def shuffle(self):
         random.shuffle(self._cards)
